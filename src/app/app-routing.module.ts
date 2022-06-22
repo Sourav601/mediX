f5e22731a6d08bcf26d8c44c2018dd73d9add2d2
Sourav601 <karmakarsourav601@gmail.com>
2022-06-22 11:16:44 +0530

    First comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from 'src/Home_Components/home-page/home-page.component';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomePageComponent},
+  {path:'login',component:LoginComponent},
+  {path:'employeeLogin',component:EmployeeLoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from '../Home_Components/carousel/carousel.component';
+import { CategoryCardsComponent } from '../Home_Components/category-cards/category-cards.component';
+import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from '../Home_Components/home-page/home-page.component';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    CategoryCardsComponent,
+    LoginComponent,
+    HomePageComponent,
+    EmployeeLoginComponent
   ],
   imports: [
     BrowserModule,
