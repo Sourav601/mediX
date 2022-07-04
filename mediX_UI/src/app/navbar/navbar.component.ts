30acbd5602f2c8caedf591ff3a88d9ce4fc89702
Sourav601 <karmakarsourav601@gmail.com>
2022-07-04 11:27:52 +0530

    LoginOrProfile Added

--- mediX_UI/src/app/navbar/navbar.component.ts
+++ mediX_UI/src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  loginOrProfile : string = 'Login';
   constructor() { }
 
   ngOnInit(): void {
