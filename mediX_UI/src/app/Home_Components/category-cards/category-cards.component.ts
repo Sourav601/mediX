f81606084e0bf3532d050876f29e8fccb3cef2a3
Suraj-Kumar <surajkumarbera09@gmail.com>
2022-07-11 12:12:58 +0530

    load home page categories from server

--- mediX_UI/src/app/Home_Components/category-cards/category-cards.component.ts
+++ mediX_UI/src/app/Home_Components/category-cards/category-cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api/api.service';
 
 @Component({
   selector: 'app-category-cards',
@@ -6,16 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-cards.component.css'],
 })
 export class CategoryCardsComponent implements OnInit {
-  categoryList: [];
-  subCategoryList: [];
+  categoryList: any;
+  subCategoryList: any;
 
-  constructor() {
-    this.categoryList = [];
-    this.subCategoryList = [];
+  constructor(private _api: ApiService) {
+    this.categoryList = null;
+    this.subCategoryList = null;
   }
 
   ngOnInit(): void {
-    console.log(this.categoryList);
-    console.log(this.subCategoryList);
+    this._api.get('api/Categories').subscribe(
+      (result: any) => {
+        this.categoryList = result;
+        console.log(this.categoryList);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+
+    this._api.get('api/Subcategories').subscribe(
+      (result: any) => {
+        this.subCategoryList = result;
+        console.log(this.subCategoryList);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
   }
 }
